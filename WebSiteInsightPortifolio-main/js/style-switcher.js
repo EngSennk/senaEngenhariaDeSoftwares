028af672dd12e678525db5c311575c95035f3fb0
EngSennk <engenheirosennk@gmail.com>
2025-01-31 14:02:46 -0300

    mudança de padrao no body de ligth para dark mode, alteração de imagens na area de Pojects Exemples com descrição, adição de Linktr.ee na area Contact me com link para todas as redes sociais.

--- WebSiteInsightPortifolio-main/js/style-switcher.js
+++ WebSiteInsightPortifolio-main/js/style-switcher.js
@@ -1,25 +1,37 @@
-// /* ============================== toggle style switcher ============================= */ 
-// const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-// styleSwitcherToggle.addEventListener("click",() => {
-//     document.querySelector(".style-switcher").classList.toggle("open");
-// })
+// ============================= toggle style switcher ==================
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler"); // Se você usa isso, mantenha. Se não, remova.
+styleSwitcherToggle.addEventListener("click", () => { // Se você usa isso, mantenha. Se não, remova.
+    document.querySelector(".style-switcher").classList.toggle("open"); // Se você usa isso, mantenha. Se não, remova.
+});
 
-// // hide style - switcher on scroll 
-// // window.addEventListener("scroll", () => {
-// //     if(document.querySelector(".style-switcher").classList.contains("open"))
-// //     {
-// //         document.querySelector(".style-switcher").classList.remove("open")
-// //     }
-// // })
-// /* ============================ theme colors ================================== */ 
-// const alternateStyles = document.querySelectorAll(".alternate-style");
 
-// function setActiveStyle(color) {
-//   alternateStyles.forEach((style) => {
-//     if (color === style.getAttribute("title")) {
-//       style.removeAttribute("disabled"); 
-//     } else {
-//       style.setAttribute("disabled", ""); 
-//     }
-//   });
-// }
+// ============================= theme colors ============================
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        } else {
+            style.setAttribute("disabled", "true");
+        }
+    });
+}
+
+/* ============================ theme light and dark mode ================================== */
+const dayNight = document.querySelector(".day-night");
+
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+});
+
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    } else {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+});
+
+// ==========================================================
